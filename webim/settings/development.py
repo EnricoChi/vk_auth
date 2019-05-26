a91a79c24a314f4895c59b48a56feab9ee9c68ea
Enrico <enricoseo@gmail.com>
2019-05-26 22:20:35 +0300

    show settings dev

---
+++ webim/settings/development.py
@@ -0,0 +1,15 @@
+import os
+
+from .base import BASE_DIR
+
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
